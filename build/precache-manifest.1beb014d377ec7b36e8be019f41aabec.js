d83e7b65c566f5e88b95bc4c8bcacbd1375ed5fb
x246072 <eduardo.saavedra@wnco.com>
2020-04-28 16:27:33 -0500

    gitignore

---
+++ build/precache-manifest.1beb014d377ec7b36e8be019f41aabec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48060587554dac02c92869fed396f3f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57a2cbc11fbdd400487c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2c43fed1d8f507bc5bb1",
+    "url": "/static/js/2.70418dde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.70418dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a2cbc11fbdd400487c",
+    "url": "/static/js/main.b3c7e9c7.chunk.js"
+  },
+  {
+    "revision": "686592871911cf6f1f0c",
+    "url": "/static/js/runtime-main.f3c9d487.js"
+  }
+]);
